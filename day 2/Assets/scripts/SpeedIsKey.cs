968f0b66333b1b062ce4da95eb978a22248cde8c
AbdullahNoumanN599 <3bdulla2005@gmail.com>
2022-07-30 14:38:32 +0300

    راسي يعورني

---
+++ day 2/Assets/scripts/SpeedIsKey.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedIsKey : MonoBehaviour
+{
+    string heroname = "jhonathon sins";
+        int heroheight = "100"
+     int heroage = "69"
+       string herosuperpower = "he can say worcestershire sauce 10 times rapidly"
+       string villianname = "barack obamos"
+        int villianheight = "5"
+        int villianage = "79"
+        string villiansuperpower = "he can do simple addition with his toes"
+        int AgeDiffrence = villianage - heroage 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
