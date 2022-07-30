453b933d88d9ef4e3286f8c5aba37255f6ad516c
AbdullahNoumanN599 <3bdulla2005@gmail.com>
2022-07-30 15:44:42 +0300

    had to do alot of error fixing i wanna cry

--- day 2/Assets/scripts/SpeedIsKey.cs
+++ day 2/Assets/scripts/SpeedIsKey.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 
 public class SpeedIsKey : MonoBehaviour
 {
-    string heroname = "jhonathon sins";
-        int heroheight = "100"
-     int heroage = "69"
-       string herosuperpower = "he can say worcestershire sauce 10 times rapidly"
-       string villianname = "barack obamos"
-        int villianheight = "5"
-        int villianage = "79"
-        string villiansuperpower = "he can do simple addition with his toes"
-        int AgeDiffrence = villianage - heroage 
     // Start is called before the first frame update
+    string heroname = "jhonathon sins";
+    int heroheight = 100;
+    int heroage = 69;
+    string herosuperpower = "he can say worcestershire sauce 10 times rapidly";
+    string villianname = "ben shortpiro";
+    int villianheight = 5;
+    int villianage = 79;
+    string villiansuperpower = "he can do simple addition with his toes";
+
     void Start()
     {
-        
+        int Agediffrence = villianage - heroage;
+        print("the hero's name is" + heroname);
+        print("he is" + heroage + "years old and his power is that" + herosuperpower);
+        print("his archnemisis" + villianname + "he is a twisted individual born" + villianage + "ago. and he uses his twisted power the abillity to" + villiansuperpower + "to not tip the waiter");
+        print("however our hero" + heroname + "has the advantage of age. the advantage of being" + Agediffrence + "years younger");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+   
+
     }
 }
